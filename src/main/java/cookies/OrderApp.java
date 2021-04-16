f042a4fc12fa45c6793b8eb5468fe0dab6c5a821
perezgws2@gmail.com <perezgws2@gmail.com>
2021-04-15 22:02:42 -0400

    Cookie orders project first draft.

--- src/main/java/cookies/OrderApp.java
+++ src/main/java/cookies/OrderApp.java
@@ -1,7 +1,31 @@
 package cookies;
 
+import java.util.Scanner;
+
 public class OrderApp {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        MasterOrder myOrder = new MasterOrder();
+        myOrder.addOrder(new CookieOrder("Tagalongs", 1));
+        myOrder.addOrder(new CookieOrder("Thin Mints", 5));
+        myOrder.addOrder(new CookieOrder("Samoas", 2));
+        myOrder.addOrder(new CookieOrder("Tagalongs", 3));
+
+        System.out.println("Current Order");
+        myOrder.showOrder();
+        System.out.println();
+        System.out.println("You have ordered " + myOrder.getTotalBoxes() + " boxes");
+        System.out.println();
+        System.out.println("What would you like to remove? (specify a variety or none)");
+        String varietyToRemove = input.nextLine();
+
+        System.out.println("You are removing " + myOrder.getVariety(varietyToRemove) + " " + varietyToRemove);
+        myOrder.removeVariety(varietyToRemove);
+
+        System.out.println("Current Order");
+        myOrder.showOrder();
 
     }
 }
